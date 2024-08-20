05c402a73f731f04513c15e17b385c66a49a7ffe
Tugado <Tugado@example.com>
2024-08-20 14:40:26 +0200

    modified:   sensor.py

--- sensor.py
+++ sensor.py
@@ -10,17 +10,9 @@ from homeassistant.const import UnitOfTemperature
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-
-
-def setup_platform(
-    hass: HomeAssistant,
-    config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None
-) -> None:
-    """Set up the sensor platform."""
-    add_entities([ExampleSensor()])
-
+from .const import DOMAIN
+from homeassistant.config_entries import ConfigEntry
+from .coordinator import ImmichDataUpdateCoordinator
 
 class ExampleSensor(SensorEntity):
     """Representation of a Sensor."""
@@ -36,3 +28,13 @@ class ExampleSensor(SensorEntity):
         This is the only method that should fetch new data for Home Assistant.
         """
         self._attr_native_value = 23
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up Dreame Vacuum sensor based on a config entry."""
+    coordinator: ImmichDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities(
+        ExampleSensor(coordinator, "description")
+    )
