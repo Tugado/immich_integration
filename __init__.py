8fb3a07c0f1b7419b2a9118e65b44f4f2446b881
Tugado <Tugado@example.com>
2024-08-20 15:07:25 +0200

    fix

--- __init__.py
+++ __init__.py
@@ -44,6 +44,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
     # Register our service with Home Assistant.
     hass.services.async_register(DOMAIN, 'refresh', refresh)
+    await hass.config_entries.async_forward_entry_setups(config, PLATFORMS)
 
     # Return boolean to indicate that initialization was successfully.
     return True
@@ -55,6 +56,7 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
     # Register our service with Home Assistant.
     hass.services.register(DOMAIN, 'pause_job', pause_job)
+
 
     # Return boolean to indicate that initialization was successfully.
     return True
