ac4413e6e75e8ebcab7079a61e2692bdac7c215f
Tugado <Tugado@example.com>
2024-08-20 14:16:56 +0200

    modified:   __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ from homeassistant.helpers.typing import ConfigType
 from .const import DOMAIN
 # TODO List the platforms that you want to support.
 # For your initial PR, limit it to 1 platform.
-PLATFORMS: list[Platform] = [Platform.LIGHT]
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 # TODO Create ConfigEntry type alias with API object
 # TODO Rename type alias and update all entry annotations
